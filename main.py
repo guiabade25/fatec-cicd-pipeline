822d01410a14b9fae245954d9e8c9680ed54a26f
Guilherme <guilhermeabade2503@gmail.com>
2026-05-28 17:53:46 -0300

    test: insere sql injection intencional

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import sqlite3
+
+
 def soma(a, b):
     return a + b
 
 
+def buscar_usuario(nome):
+    conn = sqlite3.connect("banco.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM usuarios WHERE nome = '" + nome + "'")
+    return cursor.fetchall()
+
+
 if __name__ == "__main__":
     print(soma(2, 3))
+    
